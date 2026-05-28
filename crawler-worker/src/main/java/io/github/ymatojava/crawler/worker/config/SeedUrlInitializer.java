2db9acd4dcd94382d1f4db46cd7c8de5a6d673e6
y9matoJava <xmmdkfkfxmkdkf@gmail.com>
2026-05-28 22:24:44 +0300

    feat: add SeedUrlInitializer to inject first URL on startup

---
+++ crawler-worker/src/main/java/io/github/ymatojava/crawler/worker/config/SeedUrlInitializer.java
@@ -0,0 +1,55 @@
+package io.github.ymatojava.crawler.worker.config;
+
+import io.github.ymatojava.crawler.worker.entity.UrlEntity;
+import io.github.ymatojava.crawler.worker.entity.UrlStatus;
+import io.github.ymatojava.crawler.worker.mq.UrlMessage;
+import io.github.ymatojava.crawler.worker.mq.UrlProducer;
+import io.github.ymatojava.crawler.worker.repository.UrlRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+/**
+ * Инициализатор стартовой ссылки.
+ * Выполняется автоматически при запуске Worker-а.
+ * Если база пуста, он добавляет стартовую ссылку на Википедию,
+ * чтобы краулер начал работу без ручного вмешательства.
+ */
+@Component
+public class SeedUrlInitializer implements ApplicationRunner {
+
+    private static final Logger log = Logger.getLogger(SeedUrlInitializer.class.getName());
+    
+    private final UrlRepository urlRepository;
+    private final UrlProducer urlProducer;
+
+    public SeedUrlInitializer(UrlRepository urlRepository, UrlProducer urlProducer) {
+        this.urlRepository = urlRepository;
+        this.urlProducer = urlProducer;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        if (urlRepository.count() == 0) {
+            log.info("База данных URL пуста. Добавляем Seed URL для старта краулера...");
+            
+            String seedUrl = "https://ru.wikipedia.org/wiki/Java";
+            
+            UrlEntity entity = new UrlEntity();
+            entity.setUrl(seedUrl);
+            entity.setDepth(0);
+            entity.setStatus(UrlStatus.DISCOVERED);
+            
+            entity = urlRepository.save(entity);
+            
+            UrlMessage message = new UrlMessage(entity.getId(), seedUrl, 0);
+            urlProducer.sendUrlToQueue(message);
+            
+            log.info("Seed URL успешно добавлен в базу и отправлен в очередь!");
+        } else {
+            log.info("База данных уже содержит URL. Краулер продолжит работу с текущего состояния.");
+        }
+    }
+}
